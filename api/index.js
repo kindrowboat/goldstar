647f6ab1e3bee88775d307731cfbebc18ea6de13
Kalai Wei <kawei@pivotal.io>
2019-04-16 18:03:22 -0700

    Create get & put endpoints for people
    Signed-off-by: Tom Dunlap <tdunlap@pivotal.io>

--- api/index.js
+++ api/index.js
@@ -14,7 +14,31 @@ app.post('/people', (req, res) => {
       initials: req.body.initials
     });
   });
-})
+});
+
+app.get('/people/:id', (req, res) => {
+  db.query('SELECT * FROM people WHERE id=?', [req.params.id], (_error, results, _fields) => {
+    if (results.length === 0) {
+      res.send({}, 404)
+    } else {
+      res.send(results[0]);
+    }
+  });
+});
+
+app.put('/people/:id', (req, res) => {
+  db.query('UPDATE people SET ? WHERE id=?', [{ name: req.body.name, initials: req.body.initials}, req.params.id], (_error, results, _fields) => {
+    if (results.affectedRows === 1) {
+      res.send({
+        id: results.insertId,
+        name: req.body.name,
+        initials: req.body.initials
+      });
+    } else {
+      res.send({}, 404)
+    }
+  })
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
